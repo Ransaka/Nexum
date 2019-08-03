4251e973eca5c7529153091880ff2a667cbd1153
unknown <kavindaheshan96@gmail.com>
2019-08-03 12:10:34 +0530

    test

--- server/models/log.js
+++ server/models/log.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const logSchema = new Schema({
     email: String,
+    
     password: String,
     
 })
 
 module.exports = mongoose.model('log',logSchema,'seller')
-// ajsdh
