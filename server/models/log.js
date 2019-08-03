f1190459c259b069b64ea3d9a13756fad1c4fa8f
mihirukongahage <mihirukongahage@gmail.com>
2019-08-03 12:03:52 +0530

    test

--- server/models/log.js
+++ server/models/log.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema
 const logSchema = new Schema({
     email: String,
     password: String,
-    
+
 })
 
-module.exports = mongoose.model('log',logSchema,'seller')
+module.exports = mongoose.model('log', logSchema, 'seller')
+// asdasd
