5ecaac9b0437ba4bac7848fb2cb24595016c571c
Mihiru <mihirukongahage@gmail.com>
2019-08-08 08:38:06 +0530

    landing page edits

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -1,3 +1,5 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NouisliderModule } from 'ng2-nouislider';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditDetailsComponent } from './edit-details/edit-details.component';
@@ -6,6 +8,6 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [EditDetailsComponent, UserprofileComponent],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, NouisliderModule, NgbModule]
 })
 export class PagesModule {}
