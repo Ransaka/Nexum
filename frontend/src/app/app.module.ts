d30e0a8f9e6df045c9ca2706d460f42b9bb88e9f
kavidi <48100760+kavidiii@users.noreply.github.com>
2019-08-11 12:47:41 +0530

    kavidi

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,13 +15,29 @@ import { ExamplesModule } from './examples/examples.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HttpClient } from 'selenium-webdriver/http';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { ShowCustomerDetailsComponent } from './customer_details/show-customer-details/show-customer-details.component';
+import { EditCustomerDetailsComponent } from './customer_details/edit-customer-details/edit-customer-details.component';
+import { AddNewCustomerDetailsComponent } from './customer_details/add-new-customer-details/add-new-customer-details.component';
+import { SellerItemsComponent } from './seller-items/seller-items.component';
+import { ShowItemsComponent } from './Seller_items/show-items/show-items.component';
+import { EditItemsComponent } from './Seller_items/edit-items/edit-items.component';
+import { AddNewItemComponent } from './Seller_items/add-new-item/add-new-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     UserprofileComponent,
-    BroadcastComponent
+    BroadcastComponent,
+    CustomerDetailsComponent,
+    ShowCustomerDetailsComponent,
+    EditCustomerDetailsComponent,
+    AddNewCustomerDetailsComponent,
+    SellerItemsComponent,
+    ShowItemsComponent,
+    EditItemsComponent,
+    AddNewItemComponent
   ],
   imports: [
     BrowserAnimationsModule,
