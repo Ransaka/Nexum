77308e801723c1361167afb4135831904cd0099b
Mihiru <mihirukongahage@gmail.com>
2019-08-21 23:37:48 +0530

    getbroadcast fronted

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { ExamplesModule } from './examples/examples.module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { HttpClient } from 'selenium-webdriver/http';
 import { CustomerprofileComponent } from './pages/userprofile/customerprofile/customerprofile.component';
 import { SellerprofileComponent } from './pages/userprofile/sellerprofile/sellerprofile.component';
 
--- frontend/src/app/services/broadcast.dto.ts
+++ frontend/src/app/services/broadcast.dto.ts
@@ -1,4 +1,4 @@
 export interface Broadcast {
-  category?: string;
-  textMessage?: string;
+  _id: string;
+  name?: string;
 }
