601593de17db6189412e4a2a7e8cc9b38e0ba7df
unknown <kavindaheshan96@gmail.com>
2019-08-24 12:25:04 +0530

    selling conponent created

--- frontend/src/app/components/selling/selling.component.ts
+++ frontend/src/app/components/selling/selling.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-selling',
@@ -6,10 +7,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./selling.component.scss']
 })
 export class SellingComponent implements OnInit {
-
-  constructor() { }
+  employeeForm: FormGroup;
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit() {
+    this.employeeForm = this.fb.group({
+      Laptops : this.fb.array([]),
+      Televisions : this.fb.array([]),
+      Hometheatres : this.fb.array([]),
+      Vehicles : this.fb.array([]),
+      Other : this.fb.array([])
+    })
   }
+
+  selectTheType (typeOFTheProduct : string) {
+    if (typeOFTheProduct === 'Laptops' || typeOFTheProduct === 'Televisions'){
+      (<FormArray>this.employeeForm.get('Laptops')).push(this.typeLaptopOrTelevision())
+    }if (typeOFTheProduct === 'Vehicles'){
+      (<FormArray>this.employeeForm.get('Vehicles')).push(this.typeVehicle())
+    }
+  }
+
+  typeLaptopOrTelevision (){
+    return this.fb.group({
+      BrandName : ['', Validators.required],
+      ModelName : ['', Validators.required],
+      Price : ['', Validators.required],
+      User : ['', Validators.required],
+      Performance : ['', Validators.required],
+      OtherFeatures : ['', Validators.required]
+    })
+  }
+
+  typeTelevision (){
+    console.log("Hello kavinda");
+  }
+
+  typeHometheatre (){
+
+  }
+
+  typeVehicle (){
+    return this.fb.group({
+      BrandName : ['', Validators.required],
+      Model : ['', Validators.required],
+      Price : ['', Validators.required],
+      User : ['', Validators.required],
+      EngineCapasity : ['', Validators.required],
+      OtherFeatures : ['', Validators.required]
+    })
+  }
+
+  typeOther (){
+
+  }
+
+
 
 }
