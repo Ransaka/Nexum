6b599b4803da129bf3021890468a7b4aed9a576d
kavidi <48100760+kavidiii@users.noreply.github.com>
2019-08-28 21:39:11 +0530

    kavidi

--- frontend/src/app/pages/userprofile/sellerprofile/sellerprofile.component.ts
+++ frontend/src/app/pages/userprofile/sellerprofile/sellerprofile.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit , Output, EventEmitter} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+    declare const feather: any;
+    export interface Message {
+      text: string;
+      name: string;
+    }
 @Component({
   selector: 'app-sellerprofile',
   templateUrl: './sellerprofile.component.html',
@@ -7,9 +12,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerprofileComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  @Output() onSendMessage: EventEmitter<Message> = new EventEmitter();
+  message = {
+    name: '',
+    text: '',
+  };
+  sendMessage() {
+    if (this.message.text !== '' && this.message.name !== '') {
+      this.http
+        .post(`http://localhost:4000/messages`, this.message)
+        .subscribe((res: Message) => {
+          this.onSendMessage.emit(res);
+          this.message = {
+            name: '',
+            text: '',
+          };
+        });
+    }
+  }
+  messages: Array<Message> = [];
   ngOnInit() {
+    feather.replace(); 
   }
 
 }
