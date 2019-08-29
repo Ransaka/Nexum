64695c6e10187a4cd6d6474684efc1d1ede4041f
kavidi <48100760+kavidiii@users.noreply.github.com>
2019-08-29 18:17:22 +0530

    kavidi

--- frontend/src/app/pages/ratingform/ratingform.component.ts
+++ frontend/src/app/pages/ratingform/ratingform.component.ts
@@ -1,9 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-ratingform',
   templateUrl: './ratingform.component.html',
-  styleUrls: ['./ratingform.component.scss']
+  styleUrls: ['./ratingform.component.scss'],
+  styles: [`
+    .star {
+      font-size: 1.5rem;
+      color: #b0c4de;
+    }
+    .filled {
+      color: #1e90ff;
+    }
+    .bad {
+      color: #deb0b0;
+    }
+    .filled.bad {
+      color: #ff1e1e;
+    }
+  `]
 })
 export class RatingformComponent implements OnInit {
 
@@ -13,3 +28,5 @@ export class RatingformComponent implements OnInit {
   }
 
 }
+ 
+ 
