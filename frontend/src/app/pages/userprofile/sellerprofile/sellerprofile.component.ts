fe9dc2cf64c1842fdc44f60b9fe016ec1b395db9
kavidi <48100760+kavidiii@users.noreply.github.com>
2019-09-03 22:43:25 +0530

    rate

--- frontend/src/app/pages/userprofile/sellerprofile/sellerprofile.component.ts
+++ frontend/src/app/pages/userprofile/sellerprofile/sellerprofile.component.ts
@@ -1,18 +1,37 @@
-import { Component, OnInit , Output, EventEmitter} from '@angular/core';
+
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-    declare const feather: any;
-    export interface Message {
-      text: string;
-      name: string;
-    }
+import { RatingformService } from 'app/services/ratingform.service';
+declare const feather: any;
+export interface Message {
+  text: string;
+  name: string;
+}
 @Component({
   selector: 'app-sellerprofile',
   templateUrl: './sellerprofile.component.html',
   styleUrls: ['./sellerprofile.component.scss']
 })
 export class SellerprofileComponent implements OnInit {
+  highestRate: number;
+  totalRates: number;
+  starFivePer: number;
+  starFourPer: number;
+  starThreePer: number;
+  starTwoPer: number;
+  starOnePer: number;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private rateing: RatingformService) {
+    this.highestRate = 0;
+    this.totalRates = 0;
+    this.starFivePer = 0;
+    this.starFourPer = 0;
+    this.starThreePer = 0;
+    this.starTwoPer = 0;
+    this.starOnePer = 0;
+
+
+  }
   @Output() onSendMessage: EventEmitter<Message> = new EventEmitter();
   message = {
     name: '',
@@ -33,7 +52,86 @@ export class SellerprofileComponent implements OnInit {
   }
   messages: Array<Message> = [];
   ngOnInit() {
-    feather.replace(); 
+    feather.replace();
+    this.getRatings();
+  }
+
+  getRatings() {
+
+    let currentUser = JSON.parse(localStorage.getItem('current_user'));
+    this.rateing.getRatings(currentUser._id).subscribe(
+      (res) => {
+
+        console.log(res);
+        this.calcRatings(res);
+
+      },
+      (err) => {
+
+      });
+  }
+
+  calcRatings(ratings) {
+    if (ratings) {
+      this.totalRates = ratings.length;
+      let one = 0;
+      let two = 0;
+      let three = 0;
+      let four = 0;
+      let five = 0;
+      let topCount = 0;
+
+
+      for (let i = 0; i < ratings.length; i++) {
+        if (ratings[i]) {
+
+          switch (ratings[i].rate) {
+            case 1:
+              one++;
+              this.starOnePer = one / this.totalRates * 100;
+              if (topCount < one) {
+                this.highestRate = 1;
+                topCount = one;
+              }
+
+              break;
+            case 2:
+              two++;
+              this.starTwoPer = two / this.totalRates * 100;
+              if (topCount < two) {
+                this.highestRate = 2;
+                topCount = two;
+              }
+              break;
+            case 3:
+              three++;
+              this.starThreePer = three / this.totalRates * 100;
+              if (topCount < three) {
+                this.highestRate = 3;
+                topCount = three;
+              }
+              break;
+            case 4:
+              four++;
+              this.starFourPer = four / this.totalRates * 100;
+              if (topCount < four) {
+                this.highestRate = 4;
+                topCount = four;
+              }
+              break;
+            case 5:
+              five++;
+              this.starFivePer = five / this.totalRates * 100;
+              if (topCount < five) {
+                this.highestRate = 5;
+                topCount = five;
+              }
+              break;
+          }
+        }
+      }
+
+    }
   }
 
 }
